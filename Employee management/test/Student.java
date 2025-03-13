2050ed6a31d3411abae486cd9ed656b77e9cc95e
vivekvashist44 <vivekvashist4444@gmail.com>
2025-03-13 12:16:13 +0530

    Optimize SQL query

--- Employee management/test/Student.java
+++ Employee management/test/Student.java
@@ -276,3 +276,4 @@ public class Student{
 // Commit on 2025-06-05T13:51:47 - Improve performance
 // Commit on 2025-06-05T18:50:16 - Improve performance
 // Commit on 2025-06-05T10:21:59 - Add logging
+// Commit on 2025-03-13T12:16:13 - Optimize SQL query
