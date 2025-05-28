aef172c2cf6850b232e30aaee06fb11f73d783fa
vivekvashist44 <vivekvashist4444@gmail.com>
2025-05-28 09:58:29 +0530

    Add logging

--- Employee management/test/Student.java
+++ Employee management/test/Student.java
@@ -80,3 +80,4 @@ public class Student{
 // Commit on 2025-05-28T17:34:14 - Update tests
 // Commit on 2025-05-28T20:50:08 - Update tests
 // Commit on 2025-05-28T14:47:26 - Refactor code
+// Commit on 2025-05-28T09:58:29 - Add logging
