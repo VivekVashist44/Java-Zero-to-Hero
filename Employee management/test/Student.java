d9a836f77a9315fc1b647e31e2abcec1689bdce5
vivekvashist44 <vivekvashist4444@gmail.com>
2025-07-14 17:22:08 +0530

    Update tests

--- Employee management/test/Student.java
+++ Employee management/test/Student.java
@@ -220,3 +220,4 @@ public class Student{
 // Commit on 2025-05-14T20:59:22 - Code cleanup
 // Commit on 2025-05-14T14:21:57 - Fix typo in README
 // Commit on 2025-07-14T20:59:16 - Add logging
+// Commit on 2025-07-14T17:22:08 - Update tests
