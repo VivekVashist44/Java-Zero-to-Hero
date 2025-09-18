571063d1a9cc4d7f6fba05aacb32746f5182a35f
vivekvashist44 <vivekvashist4444@gmail.com>
2025-09-18 14:36:26 +0530

    Complete Employee management

--- Employee.java
+++ Employee.java
@@ -18,56 +18,96 @@ public class Employee{
     public static void main(String []args){
 
         Scanner sc=new Scanner(System.in);
-        Employee []arr = new Employee[5];
-
-    
-        // System.out.println("Enter name");
-        // String name=sc.nextLine();
-
-        // System.out.println("Enter id");
-        // int id=sc.nextInt();
-
-        // arr[0]= new Employee(id,name);
-        // arr[1] = new Employee(2,"VK");
-
-        // System.out.println(arr[0]);
-        // System.out.println(arr[1]);
-
-
-        
+        Employee []arr = new Employee[500];
 
         int i=0;
+        boolean flag=true;
         
-        while(true){
+        while(flag){
             
-
-            System.out.println("Choose an option :");
-            System.out.println(1 +": add");
-            System.out.println("2 : remove");
-            System.out.println("3 : Update");
-            System.out.println("4 : exit");
+            System.out.println(1 +" : add");
+            System.out.println(2 +" : remove");
+            System.out.println(3 +" : Update");
+            System.out.println(4 +" : View");
+            System.out.println(5 +" : exit");
+            System.out.print("Choose an option : ");
             int option1 =sc.nextInt();
 
             switch(option1){
                 case 1:
-                    System.out.println("Enter name");
+                    System.out.print("Enter name : ");
                     String name=sc.next();
 
-                    System.out.println("Enter id");
+                    System.out.print("Enter id : ");
                     int id=sc.nextInt();
                     
                     arr[i]=new Employee(id,name);
 
-                    System.out.println("Employee added sucessuly with id =" + id + "and name =" + name);
+                    System.out.println("Employee added sucessuly with id = " + id + " and name = " + name);
                     i+=1;
                     break;
+                
+                case 2:
+                    int k=1;
+                    for(Employee val : arr){
+                        if(val==null){
+                            break;
+                        }
+                        System.out.println(k+ " : " +val);
+                        k+=1;
+                    }
+                    System.out.print("Select the employee you want to remove : ");
+
+                    int res =sc.nextInt();
+                    i-=1;
+                    System.out.println("you removed "+ arr[res-1]);
+
+                    for(int l=res-1;l<arr.length;l++){
+                        if(arr[l]==null) break;
+                        if(l==arr.length-1){
+                            arr[l]=null;
+                            break;
+                        }
+                        arr[l]=arr[l+1];
+                    }
+                    break;
+
+                case 3:
+                    int t=1;
+                    for(Employee val : arr){
+                        if(val==null){
+                            break;
+                        }
+                        System.out.println(t+ " : " +val);
+                        t+=1;
+                    }
+                    System.out.print(" Select the employee you want to update : ");
+                    
+                    res =sc.nextInt();
+
+                    System.out.print("Enter the updated name : ");
+                    String st=sc.next();
+
+                    arr[res-1]=new Employee(arr[res-1].id,st);
+                    System.out.println("Employee details are updated.");
+                    break;
+
+                case 4:
+                    for(int j=0;j<arr.length;j++){
+                        if(arr[j]==null){
+                            break;
+                        }
+                        System.out.println(arr[j]);
+                    }
+                    break;
+                
+                case 5:
+                    System.out.println("Shutting down bye bye.....");
+                    flag=false;
+                    break;
+
                 default:
-                    System.out.println("cvbnmnhgghj");
-            }
-
-
-            for(int j=0;j<arr.length;j++){
-                System.out.println(arr[j]);
+                    System.out.println("This is default case ");
             }
         }
     }
