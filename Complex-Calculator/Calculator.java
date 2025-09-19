3255bf2d03360f247b23b78f6a18242a57750998
vivekvashist44 <vivekvashist4444@gmail.com>
2025-09-19 18:35:45 +0530

    basic-changes

--- Complex-Calculator/Calculator.java
+++ Complex-Calculator/Calculator.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class Calculator{
     public static void main(String []args){
-        System.out.println("All Purpose calculator");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the expression :  ");
+        String st=sc.nextLine();
+
+        
     }
 }
