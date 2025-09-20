b6755d74c68786db2d2941b2d1fb075cc2f6f0ba
vivekvashist44 <vivekvashist4444@gmail.com>
2025-09-21 00:48:18 +0530

    few changes

---
+++ Employee management/test/Student.java
@@ -0,0 +1,8 @@
+public class Student{
+    String name="A";
+    int id=11;
+    String stndrd="B";
+    public void printvl(String name,int id, String stndrd){
+        System.out.println(name+" "+id+" "+stndrd);
+    }
+}
---
+++ Employee management/test/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String []args){
+        Student st=new Student();
+        st.name="Vivek";
+        st.id=1;
+        st.stndrd="7th";
+        st.printvl();
+        // st.printvl(name,id,stndrd);
+    }
+}
